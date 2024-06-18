93ffd894dddb2e7663342fa503c275ae59a094dd
mauri-jklein <mauri-jklein@educar.rs.gov.br>
2024-06-18 16:07:09 -0300

    Primeiro commit

---
+++ branchs/src/main/java/com/example/branchs/BranchsApplication.java
@@ -0,0 +1,13 @@
+package com.example.branchs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchsApplication.class, args);
+    }
+
+}
---
+++ branchs/src/test/java/com/example/branchs/BranchsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.branchs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
